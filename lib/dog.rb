244befe858d1e04597e85d5b7cddf05a19d6cb94
Ross Perkel <rossperkel@gmail.com>
2019-07-17 02:37:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,12 @@
 class Dog 
   def initialize(name, breed = "Mutt")
-    @name =name 
+    @name = name 
+    @breed = breed 
   end
+  
+  def breed
+    @breed
+  end
+  
+  attr_accessor :name
 end
